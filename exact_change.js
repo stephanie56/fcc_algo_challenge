86184a801a40aa54e124521fd96b6f57d5f42ccd
Stephanie Zeng <stephaniezeng521@gmail.com>
2016-12-05 01:09:23 -0500

    Updated cash register algo

--- exact_change.js
+++ exact_change.js
@@ -25,47 +25,47 @@ function findChanges(changes, arr){
   };
 
   while(changes > 0){
-    if(changes >= arr[8][1] && arr[8][1] > 0){
+    if(changes >= 100 && arr[8][1] > 0){
         changes -= 100;
         arr[8][1] -= 100;
         coinsAndBills.hundgred += 100;
     }
-    else if(changes >= arr[7][1] && arr[7][1] > 0 ){
+    else if(changes >= 20 && arr[7][1] > 0 ){
         changes -=20;
         arr[7][1] -= 20;
         coinsAndBills.twenty += 20;
     }
-    else if(changes >= arr[6][1] && arr[6][1] > 0){
+    else if(changes >= 10 && arr[6][1] > 0){
         changes -= 10;
         arr[6][1] -= 10;
         coinsAndBills.ten += 10;
     }
-    else if(changes >= arr[5][1] && arr[5][1] > 0){
+    else if(changes >= 5 && arr[5][1] > 0){
         changes -= 5;
         arr[5][1] -= 5;
         coinsAndBills.five += 5;
     }
-    else if(changes >= arr[4][1] && arr[4][1] > 0){
+    else if(changes >= 1 && arr[4][1] > 0){
         changes -= 1;
         arr[4][1] -= 1;
         coinsAndBills.one += 1;
     }
-    else if(changes >= arr[3][1] && arr[3][1] > 0){
+    else if(changes >= 0.25 && arr[3][1] > 0){
         changes -= 0.25;
         arr[3][1] -= 0.25;
         coinsAndBills.quarter += 0.25;
     }
-    else if(changes >= arr[2][1] && arr[2][1] > 0){
+    else if(changes >= 0.1 && arr[2][1] > 0){
       changes -= 0.1;
       arr[2][1] -= 0.1;
       coinsAndBills.dime += 0.1;
     }
-    else if(changes >= arr[1][1] && arr[1][1] > 0){
+    else if(changes >= 0.05 && arr[1][1] > 0){
       changes -= 0.05;
       arr[1][1] -= 0.05;
       coinsAndBills.nickel += 0.05;
     }
-    else if (changes > arr[0][1] && arr[0][1] > 0){
+    else if (changes >= 0.01 && arr[0][1] > 0){
       changes -= 0.01;
       arr[0][1] -= 0.01;
       coinsAndBills.penny += 0.01;
@@ -100,4 +100,4 @@ findChanges(changeDue, cid);
 // ["TWENTY", 60.00],twentyNum = 3
 // ["ONE HUNDRED", 100.00]] hunNum = 1
 
-checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
